961a52765f5743c77dc4d63de6194b7e4fb474fc
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-19 08:53:02 +0300

    add documentation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Contains the cities view for the API.'''
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
