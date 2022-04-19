519323943d3ba202d08ef81ef73fa31e295d2614
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-04-19 08:52:38 +0300

    add documentation

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Contains the amenities view for the API.'''
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
